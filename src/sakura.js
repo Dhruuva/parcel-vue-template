f846b8e7c45605ce7baee0034847aa600eebe9c9
Ubu20 <todhruva@mail.ru>
2022-04-26 14:51:02 +0300

    vant short method failed

---
+++ src/sakura.js
@@ -0,0 +1,10 @@
+
+
+import { createApp } from "vue";
+import ButtonCounter from "./views/cbtn.vue";
+const app = createApp({
+    template: `<ButtonCounter>Button</ButtonCounter>`,
+  });
+app.mount("#app3");
+
+
