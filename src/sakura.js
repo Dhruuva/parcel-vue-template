1e6c72b46aeecf5946ec4dbeba1bc50295af59e0
Ubu20 <todhruva@mail.ru>
2022-04-26 14:53:16 +0300

    add data test

--- src/sakura.js
+++ src/sakura.js
@@ -4,6 +4,7 @@ import { createApp } from "vue";
 import ButtonCounter from "./views/cbtn.vue";
 const app = createApp({
     template: `<ButtonCounter>Button</ButtonCounter>`,
+    data(){ return {msg:"This sakura"}}
   });
 app.mount("#app3");
 
