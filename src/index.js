57fc850e7a6b4e3bc1878188e3d574b6d86a6523
Ubu20 <todhruva@mail.ru>
2022-05-22 09:20:55 +0300

    removing old try COMMENT

--- src/index.js
+++ src/index.js
@@ -1,19 +1,9 @@
 
-//Vue.config.devtools = true
-// Before you create app
-//Vue.config.devtools = process.env.NODE_ENV === 'development'
-
-
 import { createApp } from "vue";
 import App from "./views/App.vue";
-//import Header from "./components/header.vue";
 
-// import ButtonCounter from "./views/cbtn.vue";
-// import Entry from "./views/Entry.vue";
 
 const app = createApp(App);
-//const hd = createApp(Header);
-// app.component('button-counter', ButtonCounter);
-// app.component('entry', Entry);
 app.mount("#app");
+
 //hd.mount("#mynav");
