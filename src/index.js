7703227615da0e99a76a8d619e44f46814a2f9be
Ubu20 <todhruva@mail.ru>
2022-05-24 18:27:50 +0300

    developments flag done

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 
-import { createApp } from "vue";
-import App from "./views/App.vue";
+import { createApp } from "vue/dist/vue.esm-bundler";
+import Entry from "./views/Entry.vue";
 import Header from "./components/header.vue";
-
+globalThis.__VUE_OPTIONS_API__ = true;
+globalThis.__VUE_PROD_DEVTOOLS__ = false;
 Header.props.count="232";
-const app = createApp(App);
+const app = createApp({});
 app.component("my-header",Header);
+app.component("my-entry",Entry);
+app.config.compilerOptions._VUE_OPTIONS_API__ = true;
+app.config.compilerOptions.__VUE_PROD_DEVTOOLS__ = false;
+console.log(app.config)
 app.mount("#app");
 
